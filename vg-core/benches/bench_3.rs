6007e9b59614db19318ae2c11c5b00e1205f5298
Frankie Baffa <frankiebaffa@gmail.com>
2024-03-14 17:19:58 -0400

    add criterion benchmarking

---
+++ vg-core/benches/bench_3.rs
@@ -0,0 +1,14 @@
+use {
+    vg_core::Parser,
+    criterion::{ criterion_group, criterion_main, Criterion, },
+};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("full 1 home_trim", |b| b.iter(|| Parser::compile(
+        "./test/full/1",
+        "./test/full/1/home_trim.jinja",
+    )));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ vg-core/benches/bench_5.rs
@@ -0,0 +1,14 @@
+use {
+    vg_core::Parser,
+    criterion::{ criterion_group, criterion_main, Criterion, },
+};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("full 3 page", |b| b.iter(|| Parser::compile(
+        "./test/full/3",
+        "./test/full/3/pages/page.jinja",
+    )));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
